1160269793e9ae84c401f4b9cb616225a49ecd57
asohrabpour <38889360+asohrabpour@users.noreply.github.com>
2021-02-27 13:17:30 -0500

    Delete my_ellipsoid.m

--- my_ellipsoid.m
+++
@@ -1,9 +0,0 @@
-function my_ellipsoid(h, A, O, n)
-[X, Y, Z]  = sphere(n); points  = cat(3, X, Y, Z);
-for i = 1 : n+1
-    for j = 1 : n+1
-        points(i, j, :) = (A * squeeze(points(i, j, :)) + O).';
-    end
-end
-surf(points(:, :, 1), points(:, :, 2), points(:, :, 3), 'facealpha', 0.2) 
-end

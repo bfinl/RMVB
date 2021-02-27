0e67f0e893c7d0be55e090c304be6db144098ff2
asohrabpour <38889360+asohrabpour@users.noreply.github.com>
2021-02-27 13:18:12 -0500

    Delete my_vec2str.m

--- my_vec2str.m
+++
@@ -1,9 +0,0 @@
-function str = my_vec2str(vec)
-
-str = [];
-for i = 1 : length(vec)
-    if(i == 1), str = [str, sprintf('%.2f', vec(i))];
-    else, str = [str, sprintf(',\t%.2f', vec(i))]; end
-end
-
-end
